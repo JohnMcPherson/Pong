b537e5b6a9b653dba45500f018a206b827735972
JohnMcPherson <john.mcpherson@xtra.co.nz>
2021-12-20 11:33:11 +1300

    - changes to workspace (from .gitingore?) - add constants to Pong class

--- src/Pong.java
+++ src/Pong.java
@@ -1,10 +1,16 @@
 import javax.swing.JFrame;
 
 public class Pong extends JFrame {
+    private static final String TITLE = "Pong";
+    private static final int SCREEN_WIDTH = 800;
+    private static final int SCREEN_HEIGHT = 600;
+    private static final int POINTS_NEEDED_TO_WIN = 11;
+    private static final int PADDLE_WIDTH = 6;
+    private static final int PADDLE_HEIGHT = 20;
 
     public Pong() {
-        setTitle("Pong");
-        setSize(800, 600);
+        setTitle(TITLE);
+        setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         setResizable(false);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
