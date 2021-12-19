d5e326b62f6d765ec6a113446b5196974434fe18
JohnMcPherson <john.mcpherson@xtra.co.nz>
2021-12-20 11:46:57 +1300

    - added PongPanel

---
+++ src/PongPanel.java
@@ -0,0 +1,33 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class PongPanel extends JPanel implements ActionListener, KeyListener {
+
+    public PongPanel() {
+        setBackground(Color.BLACK);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent event) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent event) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent event) {
+
+    }
+}
